164c38011052e5baf650f0090d1d8ac63d4d7bc6
KABXLO307 <151385043+KABXLO307@users.noreply.github.com>
2024-07-29 19:41:33 +0200

    Create Difference.cs

---
+++ Day One/Difference.cs
@@ -0,0 +1,2 @@
+Console.WriteLine(" What is the difference between \n  ' and a \"?  Or between a \" and a \\\"?");
+Console.WriteLine("\nOne is what we see when we're typing our program.\r\nThe other is what appears on the \"console.\"");

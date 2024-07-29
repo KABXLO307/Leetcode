4eea78761a7d698a97d7d0d6a0aef454a63300c4
KABXLO307 <151385043+KABXLO307@users.noreply.github.com>
2024-07-29 20:06:05 +0200

    Create Egg.cs

---
+++ Day One/Egg.cs
@@ -0,0 +1,15 @@
+namespace PracticeIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  _______");
+            Console.WriteLine(" /       \\");
+            Console.WriteLine("/         \\");
+            Console.WriteLine("-\"-'-\"-'-\"-");
+            Console.WriteLine("\\         /");
+            Console.WriteLine(" \\_______/");
+        }
+    }
+}
